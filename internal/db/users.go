e20f446b848cf76e6f28565276a45056cf7e577f
Lucas Crownover <lcrownover127@gmail.com>
2023-05-27 22:07:51 -0700

    fixes

--- internal/db/users.go
+++ internal/db/users.go
@@ -2,8 +2,8 @@ package db
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
 	"github.com/lcrownover/hpcadmin-server/internal/types"
+	_ "github.com/lib/pq"
 )
 
 func GetDBConnection(connectionString string) (*sql.DB, error) {
@@ -19,19 +19,4 @@ func GetUserById(db *sql.DB, id int) (*types.User, error) {
 	var user types.User
 	err := db.QueryRow("SELECT id, username, email, firstname, lastname, created_at, updated_at FROM users WHERE id = $1", id).Scan(&user.Id, &user.Username, &user.Email, &user.Firstname, &user.Lastname, &user.CreatedAt, &user.UpdatedAt)
 	return &user, err
-}
-
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	data := &UserCreate{}
-	if err := render.Bind(r, data); err != nil {
-		render.Render(w, r, ErrInvalidRequest(err))
-		return
-	}
-
-	article := data.Article
-	dbNewArticle(article)
-
-	render.Status(r, http.StatusCreated)
-	render.Render(w, r, NewArticleResponse(article))
-}
 }
